5e9d515ce575ac2c0f79604947b1e8c3595b47f1
Alba Carmona <albacarmona91@gmail.com>
2017-12-04 17:36:18 +0100

    Haciendo el objeto Persona.

---
+++ Persona/Persona/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+
+        //}
+
+        private string dni;
+        private string nombre;
+        private string apellidos;
+        private int edad;
+        private double estatura;
+        private char sexo;
+
+        
+    }
+}
