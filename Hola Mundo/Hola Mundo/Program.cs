554a6e59160e60ba7ad56f2914dcbe70c87fff17
Alba Carmona <albacarmona91@gmail.com>
2017-12-04 16:26:00 +0100

    Hola Mundo.

---
+++ Hola Mundo/Hola Mundo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola_Mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+            Console.WriteLine("Hola Alba");
+            Console.ReadKey();
+        }
+    }
+}
