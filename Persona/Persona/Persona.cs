be67576a8e41899e86fb509ab5daed66a9e60975
Alba Carmona <albacarmona91@gmail.com>
2017-12-11 16:43:50 +0100

    Hecha la interfaz Persona.

---
+++ Persona/Persona/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona
+{
+    interface Persona
+    {
+        String dni { get; set; }
+
+        String nombre { get; set; }
+
+        String apellidos { get; set; }
+
+        int edad { get; set; }
+
+        Double altura { get; set; }
+
+        Char sexo { get; set; }
+    }
+}
